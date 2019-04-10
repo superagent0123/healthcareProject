2e6f37c0bcd45c1d44d829f845e6d6defadf16fd
PC-Medisource <PC-Medisource@192.168.2.18>
2019-04-10 14:56:20 +0800

    PUSH NI JB TO

--- Scripts/Adding of Patient/Script1554877895780.groovy
+++ Scripts/Adding of Patient/Script1554877895780.groovy
@@ -299,7 +299,7 @@ WebUI.click(findTestObject('addPatient/intakeClinicalStaffCMMedisourceCM'))
 
 WebUI.delay(3)
 
-//This is to close the browser
+//This is to close the browser 123 PUSH KO TO JB
 
 WebUI.closeBrowser()
 
