fe7be85bb1b7c519f88ce9b31b0510190d958d60
Khenard Figuracion <Khenard Figuracion@192.168.2.12>
2019-05-20 14:04:35 +0800

    khen push

---
+++ Object Repository/Task_Notes/SN_Evaluation/sneval_planneddate.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>sneval_planneddate</name>
+   <tag></tag>
+   <elementGuidId>0f35295b-85f2-4e47-ab53-229931673762</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>#taskdate</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>css</name>
+      <type>Main</type>
+      <value>#taskdate</value>
+   </webElementProperties>
+</WebElementEntity>
---
+++ Object Repository/Task_Notes/SN_Evaluation/sneval_timeout.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>sneval_timeout</name>
+   <tag></tag>
+   <elementGuidId>d0751a5f-b733-46ca-8146-8fb369ca6c76</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>#timeout</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>css</name>
+      <type>Main</type>
+      <value>#timeout</value>
+   </webElementProperties>
+</WebElementEntity>
