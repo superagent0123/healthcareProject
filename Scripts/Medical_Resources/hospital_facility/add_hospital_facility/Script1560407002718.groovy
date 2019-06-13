c3a76d9a93ef89773636e2310a55981bcae2277e
Khenard <khenard.figuracion@gmail.com>
2019-06-13 16:30:40 +0800

    khenard commit

--- Scripts/Medical_Resources/hospital_facility/add_hospital_facility/Script1560407002718.groovy
+++ Scripts/Medical_Resources/hospital_facility/add_hospital_facility/Script1560407002718.groovy
@@ -12,4 +12,45 @@ import com.kms.katalon.core.testobject.TestObject as TestObject
 import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.navigateToUrl('https://qa.medisource.com/hospital')
+
+WebUI.delay(5)
+
+today = new Date()
+
+randomnum = today.format('HHmmss')
+
+WebUI.click(findTestObject('Medical_Resources/hospital_facility/hf_new_hospital_btn'))
+
+WebUI.delay(3)
+
+WebUI.click(findTestObject('Medical_Resources/hospital_facility/new_hospital_facility/newhf_typeofhf_dd'))
+
+WebUI.click(findTestObject('Medical_Resources/hospital_facility/new_hospital_facility/newhf_typeofhf_tf'))
+
+WebUI.setText(findTestObject('Medical_Resources/hospital_facility/new_hospital_facility/newhf_name'), 'Automated Hospital - ' + 
+    randomnum)
+
+WebUI.setText(findTestObject('Medical_Resources/hospital_facility/new_hospital_facility/newhf_address'), GlobalVariable.sample_address)
+
+WebUI.setText(findTestObject('Medical_Resources/hospital_facility/new_hospital_facility/newhf_city'), GlobalVariable.sample_city)
+
+WebUI.click(findTestObject('Medical_Resources/hospital_facility/new_hospital_facility/newhf_state_dd'), FailureHandling.STOP_ON_FAILURE)
+
+WebUI.sendKeys(findTestObject('Medical_Resources/hospital_facility/new_hospital_facility/newhf_state_tf'), Keys.chord('HI', 
+        Keys.ENTER))
+
+WebUI.setText(findTestObject('Medical_Resources/hospital_facility/new_hospital_facility/newhf_zipcode'), GlobalVariable.sample_ZIP_code)
+
+WebUI.setText(findTestObject('Medical_Resources/hospital_facility/new_hospital_facility/newhf_phonenumber'), GlobalVariable.sample_phone_num)
+
+WebUI.setText(findTestObject('Medical_Resources/hospital_facility/new_hospital_facility/newhf_fax'), GlobalVariable.sample_phone_num)
+
+WebUI.setText(findTestObject('Medical_Resources/hospital_facility/new_hospital_facility/newhf_contactperson'), 'Khenard Figuracion')
+
+WebUI.delay(3)
+
+WebUI.click(findTestObject('Medical_Resources/hospital_facility/new_hospital_facility/newhf_savebtn'), FailureHandling.STOP_ON_FAILURE)
 
